7c344041ed832c835b392ef172b24d768faa3527
wyk <wyk_914@163.com>
2022-05-25 15:48:39 +0800

    First Commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author Salted Fish
+ * @create 2022-05-25 15:44
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
